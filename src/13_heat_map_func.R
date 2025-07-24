ce0111da6d9db0798ba7da2d7cfeeffbe200b6d5
Josh Culpepper <jculpepper@nevada.unr.edu>
2025-07-23 20:23:56 -0400

    -Fixed the map code for script 13

--- src/13_heat_map_func.R
+++ src/13_heat_map_func.R
@@ -1,26 +1,36 @@
-ggplot() +
-  geom_tile(
-    pls_temp, 
-    mapping = aes(yday, depth, fill = temp)
-  ) +
-  scale_y_reverse() +
-  scale_fill_wa_c(
-    palette = 'lopez',
-    reverse = T
-  )+
-  coord_cartesian(expand = FALSE)+
+heat_map <- function(
+    df, 
+    xvar, 
+    yvar, 
+    map_var, #This is the color variable for the heat map e.g., 'temp_c'
+    lake, #this is for the site id e.g., 'paint.shallow'
+    facet #this should just be 'year'
+  ) {
   
-  geom_vline(
-    rbr_df %>% filter(site == 'paint.shallow'),
-    mapping = aes(xintercept = yday),
-    linewidth = 2,
-    linetype = 'dotted',
-    color = 'black',
-    alpha = 0.2
-  )+
-  facet_wrap(
-    ~year, 
-    #scales = 'free', 
-    ncol = 1
-  )+
-  theme_classic()
+  ggplot() +
+    geom_tile(
+      df, 
+      mapping = aes(xvar, yvar, fill = map_var)
+    ) +
+    scale_y_reverse() +
+    scale_fill_wa_c(
+      palette = 'lopez',
+      reverse = T
+    )+
+    coord_cartesian(expand = FALSE)+
+    
+    geom_vline(
+      rbr_df %>% filter(site == {{lake}}),
+      mapping = aes(xintercept = xvar),
+      linewidth = 2,
+      linetype = 'dotted',
+      color = 'black',
+      alpha = 0.2
+    )+
+    facet_wrap(
+      ~{{facet}}, 
+      #scales = 'free', 
+      ncol = 2
+    )+
+    theme_classic()
+}
